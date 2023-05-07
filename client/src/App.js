3f80741b74f6ce5250d84fd715790e3aca039544
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-07 11:03:40 +0300

    Add blank home page

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,18 @@ import {
   Login, 
   Logout, 
 } from './pages/Account';
+import Home from './pages/Home';
 import Header from './pages/Header';
 
   
 function App() {
   return (
     <>
-    <Header  />
     <div className="container-custom">
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
     </>
