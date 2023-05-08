ef5d7f6cf1c269a823635a34530dc8c9ac57f7b8
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-08 14:41:38 +0300

    Create AccountDTO similar to backend

---
+++ client/src/dto/AccountDTO.js
@@ -0,0 +1,13 @@
+class AccountDTO {
+    constructor(firstName, lastName, role, username, password, gallery) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.role = role;
+      this.username = username;
+      this.password = password;
+      this.gallery = gallery;
+    }
+  }
+  
+  export default AccountDTO;
+  
